dcc5cfcb26d61cf576a4237c523946610aca2f87
Gabriel Sanchez <shgabriel06@gmail.com>
2026-02-13 23:23:05 -0500

    feat: verificar y conectar con la base de datos

--- src/main/java/bodega/Main.java
+++ src/main/java/bodega/Main.java
@@ -1,10 +1,23 @@
 package bodega;
 
+import bodega.database.DatabaseConnection;
 import bodega.vista.ContenedorFrame;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class Main {
     void main() {
         IO.println("¡Hola Mundo!");
+        try {
+            Connection cn = DatabaseConnection.getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            javax.swing.JOptionPane.showMessageDialog(null,
+                    "No se pudo conectar a la base de datos.\n" + "Error: " + e.getMessage(),
+                    "Error de Conexión", javax.swing.JOptionPane.ERROR_MESSAGE);
+            System.exit(0);
+        }
         ContenedorFrame contenedorFrame = new ContenedorFrame();
         contenedorFrame.setVisible(true);
     }
